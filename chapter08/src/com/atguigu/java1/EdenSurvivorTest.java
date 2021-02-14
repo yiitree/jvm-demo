d39868c63a1aff4adc5633f2b30e3fe9929a4ebf
zengrui <772701414@qq.com>
2021-02-14 21:04:33 +0800

    init

--- chapter08/src/com/atguigu/java1/EdenSurvivorTest.java
+++ chapter08/src/com/atguigu/java1/EdenSurvivorTest.java
@@ -5,7 +5,7 @@ package com.atguigu.java1;
  *
  * -XX:NewRatio ： 设置新生代与老年代的比例。默认值是2.
  * -XX:SurvivorRatio ：设置新生代中Eden区与Survivor区的比例。默认值是8
- * -XX:-UseAdaptiveSizePolicy ：关闭自适应的内存分配策略  （暂时用不到）
+ * -XX:-UseAdaptiveSizePolicy ：关闭自适应的内存分配策略  （暂时用不到，及时关闭了但是也不行）
  * -Xmn:设置新生代的空间的大小。 （一般不设置）
  *
  * @author shkstart  shkstart@126.com
