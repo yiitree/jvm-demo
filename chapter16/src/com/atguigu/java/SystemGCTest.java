07c3e484811b52853f9b3529c93ec3b63337254b
zengrui <772701414@qq.com>
2021-03-14 23:41:44 +0800

    垃圾回收

--- chapter16/src/com/atguigu/java/SystemGCTest.java
+++ chapter16/src/com/atguigu/java/SystemGCTest.java
@@ -10,7 +10,10 @@ public class SystemGCTest {
         System.gc();//提醒jvm的垃圾回收器执行gc,但是不确定是否马上执行gc
         //与Runtime.getRuntime().gc();的作用一样。
 
+        // finalize()就算多次调用，也只会执行一次
         System.runFinalization();//强制调用使用引用的对象的finalize()方法
+        System.runFinalization();
+        System.runFinalization();
     }
 
     @Override
